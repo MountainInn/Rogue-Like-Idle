a64b31868b8b11a53ec9d61a2bbc363ad4697f0e
Mountain <blckclst@gmail.com>
2022-12-31 17:19:59 +0500

    Add Ref<T> - reference wrapper for unmanaged types

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -108,21 +108,3 @@ public partial class Unit
         static public implicit operator double(Stat stat) => stat.Result.Value;
     }
 }
-
-public class Ref<T>
-    where T : unmanaged
-{
-
-    public T Value {get ; private set; }
-
-    public Ref(T value)
-    {
-        Value = value;
-    }
-
-    static public implicit operator T(Ref<T> reft) => reft.Value;
-    static public implicit operator Ref<T>(T t)
-    {
-        return new Ref<T>(t);
-    }
-}
