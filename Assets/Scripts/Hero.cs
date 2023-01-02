4bd5402f0d38e4a7f1ab806f990ae929cb836599
Mountain <blckclst@gmail.com>
2023-01-02 21:01:55 +0500

    Add [Inject] to Hero.Awake so it initializes in injection stage, along with the rest injection methods

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Newtonsoft.Json;
+using Zenject;
 
 [JsonObjectAttribute]
 public class Hero : MonoBehaviour
@@ -9,14 +10,16 @@ public class Hero : MonoBehaviour
     [JsonPropertyAttribute] public Expiriense expiriense {get; private set;}
     [JsonPropertyAttribute] public Unit unit {get; private set;}
 
-    private Awake()
+    [Inject]
+    private void Awake()
     {
         Level level = new Level(1);
-        this.unit.level = level;
+
         this.expiriense = new Expiriense(level, (level)=> level * 100);
-    }
 
         this.unit = new Unit(10, 10, null);
+        this.unit.level = level;
+    }
 
     static public implicit operator Unit(Hero hero) => hero.unit;
 }
