24290df2439d5f4ceded88a419881874dbdd933b
Mountain <blckclst@gmail.com>
2023-01-04 21:28:51 +0500

    Add extension method to format double

--- Assets/Scripts/Ext.cs
+++ Assets/Scripts/Ext.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Globalization;
 using UnityEngine;
 
 namespace MountainInn
@@ -19,5 +20,21 @@ namespace MountainInn
         {
             return Input.mousePosition * canvas.transform.lossyScale.x;
         }
+    }
+}
+
+static public class DoubleExt
+{
+    static public string BeautifulFormat(this double number)
+    {
+        double abs = System.Math.Abs(number);
+
+        string str =
+            (abs > 9_999_999f)
+            ? abs.ToString("e2")
+            : abs.ToString("N0", CultureInfo.InvariantCulture);
+
+
+        return str ;
     }
 }
