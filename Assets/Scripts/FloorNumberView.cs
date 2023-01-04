6cf93187e2b641089255503ae4fc485e6b755774
Mountain <blckclst@gmail.com>
2023-01-04 21:30:18 +0500

    Make FloorNumberView display floor number on start

--- Assets/Scripts/FloorNumberView.cs
+++ Assets/Scripts/FloorNumberView.cs
@@ -5,13 +5,19 @@ using TMPro;
 public class FloorNumberView : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI floorNumberText;
+    private DungeonFloor dungeonFloor;
 
     [Inject]
     public void Construct(DungeonFloor dungeonFloor)
     {
+        this.dungeonFloor = dungeonFloor;
         dungeonFloor.onFloorNumberUp += DisplayFloorNumber;
     }
 
+    private void Start()
+    {
+        DisplayFloorNumber(dungeonFloor.floorNumber);
+    }
     private void DisplayFloorNumber(uint number)
     {
         floorNumberText.text = $"Floor {number}".PadRight(10);
