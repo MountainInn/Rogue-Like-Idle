b82514b940f7be67b9062dd24ad9c4ad35c1aa28
MountainInn <blckclst@gmail.com>
2023-01-06 20:05:53 +0500

    Add initialization of talents and talent tree

--- Assets/Scripts/TalentTree.cs
+++ Assets/Scripts/TalentTree.cs
@@ -14,8 +14,55 @@ public class TalentTree : MonoBehaviour, IEnumerable
 
     public event Action<Unit.Skill> onTalentUnlocked;
 
-    public TalentTree()
+    List<Node<Unit.Talent>>
+        branches;
+
+    Transform
+        talentViewPrefab;
+    Transform
+        content;
+
+    [Inject]
+    public void Construct(TalentView talentViewPrefab)
     {
+        this.talentViewPrefab = talentViewPrefab;
+    }
+
+   
+    private void Awake()
+    {
+        content = GetComponent<ScrollView>().content;
+
+        var simpleStrike = new Unit.SimpleStrikeTalent(5);
+        var improvedSimpleStrike = new Unit.ImprovedSimpleStrikeTalent(5);
+        var healing = new Unit.HealingTalent(5);
+        var challengeWeakness = new Unit.ChallengeTalent_Weakness(10);
+
+        branches = new List<Node<Unit.Talent>>(3);
+
+        branches[0] =
+            new Node<Unit.Talent>(simpleStrike)
+            {
+                new Node<Unit.Talent>(improvedSimpleStrike),
+                new Node<Unit.Talent>(healing)
+                {
+                    challengeWeakness
+                }
+            };
+        branches[0].ConstructTree();
+
+    }
+
+    private void DisplayTree()
+    {
+        branches[0].ForEach(
+            (node)=>
+            {
+                var newTalentView = GameObject.Instantiate(talentViewPrefab, content);
+
+                node.SetViewPosition(newTalentView, 15);
+
+            });
 
     }
 
