dc6b973a1230f915fab3115eb367cd0f9e9d5878
Mountain <blckclst@gmail.com>
2023-01-13 21:28:45 +0500

    Remove IEnumerable interface from TalentTree

--- Assets/Scripts/TalentTree.cs
+++ Assets/Scripts/TalentTree.cs
@@ -4,14 +4,10 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class TalentTree : MonoBehaviour, IEnumerable
+public class TalentTree : MonoBehaviour
 {
     [SerializeField]
     public List<Unit.Talent> talents;
-
-    [HideInInspector]
-    public List<Unit.Skill> skills;
-
     public event Action<Unit.Skill> onTalentUnlocked;
 
     List<Node<Unit.Talent>>
@@ -66,34 +62,9 @@ public class TalentTree : MonoBehaviour, IEnumerable
 
     }
 
-    public IEnumerator GetEnumerator()
+    private void Start()
     {
-        return skills.GetEnumerator();
+        DisplayTree();
     }
 
-    public void Add(Unit.Skill rootSkill)
-    {
-        skills.Add(rootSkill);
-    }
-
-    public void Add(Unit.Skill descendant, Type ancestorType, int requiredLevel)
-    {
-        Add(descendant);
-
-        var ancestor = skills.Single(skill => skill.GetType() == ancestorType);
-
-        if (ancestor == null)
-            throw new System.Exception($"Ancestor of type {ancestorType} not found in TalentTree.");
-
-        ancestor.onLevelUp += CheckRequirement;
-
-        void CheckRequirement(int level)
-        {
-            if (level >= requiredLevel)
-            {
-                onTalentUnlocked?.Invoke(descendant);
-                ancestor.onLevelUp -= CheckRequirement;
-            }
-        }
-    }
 }
