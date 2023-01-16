677dc912d64a5747b4463b4a6329ce81b22cc7c7
MountainInn <blckclst@gmail.com>
2023-01-16 17:56:53 +0500

    Add maxFloor registration to DungeonFloor class

--- Assets/Scripts/DungeonFloor.cs
+++ Assets/Scripts/DungeonFloor.cs
@@ -6,6 +6,7 @@ using Zenject;
 public class DungeonFloor
 {
     [JsonPropertyAttribute] public uint floorNumber;
+    [JsonPropertyAttribute] public uint maxFloorNumber;
 
     private DungeonFloorView view;
     private Battle battle;
@@ -13,6 +14,7 @@ public class DungeonFloor
 
     public event Action<uint> onFloorNumberUp;
     public event Action<uint> onFloorNumberDown;
+    public event Action<uint> onMaxFloorChanged;
 
     [Inject]
     public DungeonFloor(DungeonFloorView view, Battle battle)
@@ -26,6 +28,14 @@ public class DungeonFloor
     public void UpFloorNumber()
     {
         floorNumber++;
+
+        if (maxFloorNumber < floorNumber)
+        {
+            maxFloorNumber = floorNumber;
+
+            onMaxFloorChanged?.Invoke(maxFloorNumber);
+        }
+
         onFloorNumberUp?.Invoke(floorNumber);
     }
 
